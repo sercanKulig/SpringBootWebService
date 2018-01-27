1bcd256927857ef3669fbd28ee82d2d227c2b39a
sercanKuliq <sercan.kulig@gmail.com>
2018-01-27 23:14:04 +0300

    ws start

---
+++ src/main/java/com/example/wsservice/WsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.wsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsServiceApplication.class, args);
+    }
+}
